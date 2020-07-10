83ed1cb6fad8424557e89cf53834305df8b468fc
Ryan Priebe <ryan@yellowpencil.com>
2020-07-09 21:29:30 -0600

    latest changes improve UX a bit by adding some field value checking and by adding a new field for job ID

--- DLPrototype/ContentView.swift
+++ DLPrototype/ContentView.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 YegCollective. All rights reserved.
 //
 
+import Combine
 import SwiftUI
 
 struct Category: Identifiable {
@@ -20,6 +21,8 @@ struct ContentView: View {
         categories.append(Category(title: "Daily"))
         categories.append(Category(title: "Standup"))
         categories.append(Category(title: "Reflection"))
+        
+        createLogFiles()
     }
     
     var body: some View {
@@ -47,7 +50,15 @@ struct ContentView: View {
             }
             .navigationViewStyle(DoubleColumnNavigationViewStyle())
         }
-//        .frame(minWidth: 900, minHeight: 900)
+//        .frame(minWidth: 700, minHeight: 700)
+    }
+    
+    func createLogFiles() -> URL {
+        let paths = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)
+        
+        
+        
+        return paths[0]
     }
 }
 
@@ -55,33 +66,91 @@ struct AddView : View {
     var category: Category
     
     @State private var text: String = ""
-    @State private var showingAlert = false
+    @State private var jobId: Int = 0
+    @State private var noLogMessageAlert = false
+    @State private var noJobIdAlert = false
     
     var body: some View {
         VStack {
             Text(category.title)
                 .font(.title)
             
-            TextField("Enter your daily log text here", text: $text)
+            HStack {
+                NumberEntryField(title: "Job ID", value: self.$jobId)
+                    .frame(width: 100)
+                
+                TextField("Enter your daily log text here", text: $text)
+            }
             
             Spacer()
             
             Button("Log", action: {
                 if self.$text.wrappedValue.count > 0 {
-                    print("Logged \(self.$text.wrappedValue)")
-                    self.$text.wrappedValue = ""
+                    if self.$jobId.wrappedValue > 0 {
+                        self.logLine()
+                        
+                        self.$text.wrappedValue = ""
+                        self.$jobId.wrappedValue = 0
+                    } else {
+                        self.noJobIdAlert = true
+                    }
                 } else {
                     print("You have to type something")
-                    self.showingAlert = true
+                    self.noLogMessageAlert = true
                 }
+                
+                
             })
-                .alert(isPresented:$showingAlert) {
-                    Alert(title: Text("You must enter some text"), message: Text("You cannot log an empty string"), primaryButton: .default(Text("OK")), secondaryButton: .cancel()
+                .alert(isPresented: $noLogMessageAlert) {
+                    Alert(title: Text("Log text is a required field"), message: Text("You cannot log an empty string"), primaryButton: .default(Text("OK")), secondaryButton: .cancel()
+                    )
+                }
+                .alert(isPresented: $noJobIdAlert) {
+                    Alert(title: Text("Job ID is a required field"), message: Text("There is no job ID 0"), primaryButton: .default(Text("OK")), secondaryButton: .cancel()
                     )
                 }
         }
-            .frame(width: 900, height: 900)
+            .frame(width: 700, height: 700)
             .padding()
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)
+        
+        return paths[0]
+    }
+    
+    func logLine() -> Void {
+        print("Logged: \(self.$text.wrappedValue)")
+        
+//        let fileName = Bundle.main.path(forResource: category.title, ofType: "log")
+        let fileName = "/Rolling Logs/\(category.title).log"
+        let filePath = getDocumentsDirectory().appendingPathComponent(fileName)
+        print(fileName)
+        
+        do {
+            try self.$text.wrappedValue.write(to: filePath, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            print("Unable to write to file")
+        }
+    }
+}
+
+// thanks https://stackoverflow.com/a/61236221/7044855
+struct NumberEntryField : View {
+    @State private var enteredValue : String = ""
+    
+    var title: String
+    
+    @Binding var value: Int
+
+    var body: some View {
+        return TextField(title, text: $enteredValue)
+            .onReceive(Just(enteredValue)) { typedValue in
+                if let newValue = Int(typedValue) {
+                    self.value = newValue
+                }
+        }.onAppear(perform:{self.enteredValue = "\(self.value)"})
     }
 }
 
@@ -96,20 +165,20 @@ struct LogView: View {
             Spacer()
             
             ScrollView {
-                Text(readFile(whichLog: category.title))
+                Text(readFile())
             }
             
             Spacer()
         }
-        .frame(width: 900, height: 900)
+        .frame(width: 700, height: 700)
         .padding()
     }
     
-    func readFile(whichLog: String) -> String {
+    func readFile() -> String {
         var lines: String = "nothing to see here"
 
-        if let dailyLog = Bundle.main.url(forResource: whichLog, withExtension: "log") {
-            if let logLines = try? String(contentsOf: dailyLog) {
+        if let log = Bundle.main.url(forResource: category.title, withExtension: "log") {
+            if let logLines = try? String(contentsOf: log) {
                 if !logLines.isEmpty {
                     lines = logLines
                 }
