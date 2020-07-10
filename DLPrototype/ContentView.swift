5a143b5a9bb4315684b0204329807427b50a6644
Ryan Priebe <ryan@yellowpencil.com>
2020-07-10 12:23:16 -0600

    better UI, removed unused resource files

--- DLPrototype/ContentView.swift
+++ DLPrototype/ContentView.swift
@@ -60,53 +60,38 @@ struct AddView : View {
     var category: Category
     
     @State private var text: String = ""
-    @State private var jobId: Int = 0
+    @State private var jobId: String = ""
     @State private var noLogMessageAlert = false
     @State private var noJobIdAlert = false
     
     var body: some View {
-        VStack {
-            Text(category.title)
+        VStack(alignment: .leading) {
+            Text("Append to \(category.title).log")
                 .font(.title)
             
             HStack {
-                NumberEntryField(title: "Job ID", value: self.$jobId)
+                TextField("Job ID", text: self.$jobId)
                     .frame(width: 100)
                 
                 TextField("Enter your daily log text here", text: $text)
+                
+                Button("Log", action: {
+                    if self.$text.wrappedValue != "" && self.$jobId.wrappedValue != "" {
+                        self.logLine()
+                        
+                        self.$text.wrappedValue = ""
+                        self.$jobId.wrappedValue = ""
+                    } else {
+                        print("You have to type something")
+                    }
+                })
             }
             
             Spacer()
             
-            HStack {
-                Button("Log", action: {
-                    if self.$text.wrappedValue.count > 0 {
-                        if self.$jobId.wrappedValue > 0 {
-                            self.logLine()
-                            
-                            self.$text.wrappedValue = ""
-                            self.$jobId.wrappedValue = 0
-                        } else {
-                            self.noJobIdAlert = true
-                        }
-                    } else {
-                        print("You have to type something")
-                        self.noLogMessageAlert = true
-                    }
-                })
-                    .alert(isPresented: $noLogMessageAlert) {
-                        Alert(title: Text("Log text is a required field"), message: Text("You cannot log an empty string"), primaryButton: .default(Text("OK")), secondaryButton: .cancel()
-                        )
-                    }
-                    .alert(isPresented: $noJobIdAlert) {
-                        Alert(title: Text("Job ID is a required field"), message: Text("There is no job ID 0"), primaryButton: .default(Text("OK")), secondaryButton: .cancel()
-                        )
-                    }
-                
-                Button("New Day", action: {
-                    self.logNewDay()
-                })
-            }
+            Button("New Day", action: {
+                self.logNewDay()
+            })
         }
             .frame(width: 700, height: 700)
             .padding()
@@ -119,20 +104,13 @@ struct AddView : View {
     }
     
     func writeToLog(output: Data) -> Void {
-        print("Logged: \(self.$text.wrappedValue)")
-        
         let fileName = "\(category.title).log"
         let filePath = getDocumentsDirectory().appendingPathComponent(fileName)
         
-        
-        do {
-            if let fileHandle = try? FileHandle(forWritingTo: filePath) {
-                fileHandle.seekToEndOfFile()
-                fileHandle.write(output)
-                fileHandle.closeFile()
-            }
-        } catch {
-            print("Unable to write to file")
+        if let fileHandle = try? FileHandle(forWritingTo: filePath) {
+            fileHandle.seekToEndOfFile()
+            fileHandle.write(output)
+            fileHandle.closeFile()
         }
     }
     
@@ -145,27 +123,9 @@ struct AddView : View {
     
     func logLine() -> Void {
         let time = Date()
-        guard let line: Data = ("\(time) - \(self.$jobId.wrappedValue) - \(self.$text.wrappedValue)").data(using: String.Encoding.utf8) else { return }
+        guard let line: Data = ("\n\(time) - \(self.$jobId.wrappedValue) - \(self.$text.wrappedValue)").data(using: String.Encoding.utf8) else { return }
         
         writeToLog(output: line)
-    }
-}
-
-// thanks https://stackoverflow.com/a/61236221/7044855
-struct NumberEntryField : View {
-    @State private var enteredValue : String = ""
-    
-    var title: String
-    
-    @Binding var value: Int
-
-    var body: some View {
-        return TextField(title, text: $enteredValue)
-            .onReceive(Just(enteredValue)) { typedValue in
-                if let newValue = Int(typedValue) {
-                    self.value = newValue
-                }
-        }.onAppear(perform:{self.enteredValue = "\(self.value)"})
     }
 }
 
@@ -173,8 +133,8 @@ struct LogView: View {
     var category: Category
     
     var body: some View {
-        VStack {
-            Text("Viewing \(category.title) Log")
+        VStack(alignment: .leading) {
+            Text("\(category.title).log")
                 .font(.title)
             
             Spacer()
@@ -198,7 +158,6 @@ struct LogView: View {
     func readFile() -> String {
         var lines: String = "nothing to see here"
 
-//        if let log = Bundle.main.url(forResource: category.title, withExtension: "log") {
         let log = getDocumentsDirectory().appendingPathComponent("\(category.title).log")
             
         if let logLines = try? String(contentsOf: log) {
