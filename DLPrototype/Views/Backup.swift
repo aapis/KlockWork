934e1258f19b1ab301d85f6912f7ad8e594bcb84
Ryan Priebe <rpriebe@me.com>
2020-11-15 19:35:21 -0700

    backup now functional

--- DLPrototype/Views/Backup.swift
+++ DLPrototype/Views/Backup.swift
@@ -9,6 +9,8 @@
 import SwiftUI
 
 struct Backup: View {
+    @State private var lastBackupDate: String = ""
+    
     var category: Category
     
     var body: some View {
@@ -22,7 +24,7 @@ struct Backup: View {
             
             Divider()
             
-            Text("Last Backup: ") + Text(self.getLastBackupDate())
+            Text("Last Backup: ") + Text($lastBackupDate.wrappedValue)
             
             Button("Backup Now", action: {
                 self.performBackup()
@@ -33,31 +35,63 @@ struct Backup: View {
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
         .padding()
+        .onAppear(perform: getLastBackupDate)
     }
     
-    func getDocumentsDirectory() -> URL {
+    private func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask)
         
         return paths[0]
     }
     
-    func getLastBackupDate() -> String {
+    private func getLastBackupDate() -> Void {
         let log = getDocumentsDirectory().appendingPathComponent("\(category.title).log")
         
         if let logLines = try? String(contentsOf: log) {
             let lines = logLines.components(separatedBy: .newlines)
             
             if lines.count > 0 {
-                return lines.first!
+                $lastBackupDate.wrappedValue = lines.first!
             }
         }
-        
-        return "N/A"
     }
     
-    func performBackup() -> Void {
-        let today = Date()
+    private func performBackup() -> Void {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
         
+        let date = formatter.string(from: Date())
+        let to = getDocumentsDirectory().appendingPathComponent("Log Backups/\(category.title)-\(date).log")
+        let at = getDocumentsDirectory().appendingPathComponent("\(category.title).log")
         
+        if let _ = try? FileManager().copyItem(at: at, to: to) {
+            let text = "Backed up to \"Log Backups/\(date)\""
+            
+            logLine(text: text)
+            
+            $lastBackupDate.wrappedValue = date
+        }
+    }
+    
+    private func writeToLog(output: Data) -> Void {
+        let fileName = "\(category.title).log"
+        let filePath = getDocumentsDirectory().appendingPathComponent(fileName)
+        
+        if let fileHandle = try? FileHandle(forWritingTo: filePath) {
+            fileHandle.truncateFile(atOffset: 0)
+            fileHandle.write(output)
+            fileHandle.closeFile()
+        }
+    }
+    
+    private func logLine(text: String) -> Void {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let date = formatter.string(from: Date())
+        
+        guard let line: Data = ("\n\(date) - \(text)").data(using: String.Encoding.utf8) else { return }
+        
+        writeToLog(output: line)
     }
 }
