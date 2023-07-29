f8f756c6bf695227df81d91ae8491aaeb23809e7
Ryan Priebe <rpriebe@me.com>
2023-07-28 18:24:03 -0600

    added items to main menu to create items

---
+++ DLPrototype/Views/Jobs/JobCreate.swift
@@ -0,0 +1,21 @@
+//
+//  JobCreate.swift
+//  DLPrototype
+//
+//  Created by Ryan Priebe on 2023-07-28.
+//  Copyright © 2023 YegCollective. All rights reserved.
+//
+
+import SwiftUI
+
+struct JobCreate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JobCreate_Previews: PreviewProvider {
+    static var previews: some View {
+        JobCreate()
+    }
+}
