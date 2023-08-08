3618bace623d2b099cfd6f2340554d6654c6911b
Ryan Priebe <rpriebe@me.com>
2023-08-07 22:16:58 -0600

    changes some of the icons hover states a little bit (probably broke a few yolo)

--- DLPrototype/Views/Shared/AppSidebar/SidebarItem.swift
+++ DLPrototype/Views/Shared/AppSidebar/SidebarItem.swift
@@ -78,6 +78,7 @@ struct SidebarItem: View, Identifiable {
     public var showButton: Bool = true
 
     @State private var highlighted: Bool = false
+    @State private var altIcon: String?
     
     var body: some View {
         HStack(alignment: .top, spacing: 0) {
@@ -93,9 +94,8 @@ struct SidebarItem: View, Identifiable {
         .mask(
             RoundedRectangle(cornerRadius: 4)
         )
-        .onHover { inside in
-            highlighted.toggle()
-        }
+        .useDefaultHover({ inside in highlighted = inside})
+        .onAppear(perform: actionOnAppear)
         .contextMenu {
             Button("Copy \(data)") {
                 ClipboardHelper.copy(data)
@@ -113,12 +113,24 @@ struct SidebarItem: View, Identifiable {
                 }
 
                 Button(action: doAction) {
-                    Image(systemName: ic)
-                        .font(.title2)
-                        .frame(maxWidth: 30, maxHeight: 30)
+                    if let alt = altIcon {
+                        if highlighted {
+                            Image(systemName: alt)
+                                .font(.title2)
+                                .frame(maxWidth: 30, maxHeight: 30)
+                        } else {
+                            Image(systemName: ic)
+                                .font(.title2)
+                                .frame(maxWidth: 30, maxHeight: 30)
+                        }
+                    } else {
+                        Image(systemName: ic)
+                            .font(.title2)
+                            .frame(maxWidth: 30, maxHeight: 30)
+                    }
                 }
                 .buttonStyle(.plain)
-                .useDefaultHover({ inside in highlighted = inside})
+                .useDefaultHover({_ in})
             }
             .frame(width: type.iconFrameSize)
         }
@@ -144,6 +156,12 @@ extension SidebarItem {
     private func doAction() -> Void {
         if let callback = action {
             callback()
+        }
+    }
+
+    private func actionOnAppear() -> Void {
+        if let ic = icon {
+            altIcon = ic + ".fill"
         }
     }
 }
