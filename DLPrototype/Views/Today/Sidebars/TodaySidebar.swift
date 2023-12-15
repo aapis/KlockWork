1dcca6b8ad18124fc26cdf2429d97f778c1bddcb
Ryan Priebe <rpriebe@me.com>
2023-12-15 13:30:15 -0700

    converted today sidebar to an omni sidebar with tab driven UX

--- DLPrototype/Views/Today/Sidebars/TodaySidebar.swift
+++ DLPrototype/Views/Today/Sidebars/TodaySidebar.swift
@@ -10,26 +10,58 @@ import SwiftUI
 
 struct TodaySidebar: View {
     @State public var date: Date = Date()
+    @State private var tabs: [ToolbarButton] = []
 
     @EnvironmentObject public var nav: Navigation
 
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack(alignment: .leading, spacing: 5) {
-                JobPickerWidget()
-                TasksWidget()
-                NoteSearchWidget()
+                FancyGenericToolbar(buttons: tabs, standalone: true, location: .sidebar)
             }
             Spacer()
         }
         .padding()
+        .onAppear(perform: createToolbar)
     }
 }
 
 extension TodaySidebar {
-    private func formattedDate() -> String {
-        let df = DateFormatter()
-        df.dateFormat = "MMM d, yyyy"
-        return df.string(from: nav.session.date)
+    private func createToolbar() -> Void {
+        tabs = [
+            ToolbarButton(
+                id: 0,
+                helpText: "Jobs",
+                label: AnyView(
+                    HStack {
+                        Image(systemName: "hammer").padding(.leading)
+                        Text("Jobs")
+                    }
+                ),
+                contents: AnyView(JobPickerWidget())
+            ),
+            ToolbarButton(
+                id: 1,
+                helpText: "Tasks",
+                label: AnyView(
+                    HStack {
+                        Image(systemName: "checklist").padding(.leading)
+                        Text("Tasks")
+                    }
+                ),
+                contents: AnyView(TasksWidget())
+            ),
+            ToolbarButton(
+                id: 2,
+                helpText: "Notes",
+                label: AnyView(
+                    HStack {
+                        Image(systemName: "note.text").padding(.leading)
+                        Text("Notes")
+                    }
+                ),
+                contents: AnyView(NoteSearchWidget())
+            )
+        ]
     }
 }
