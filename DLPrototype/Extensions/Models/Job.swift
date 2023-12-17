c7d43d02c835885ab8109ecd56bebbc2562314de
Ryan Priebe <rpriebe@me.com>
2023-12-17 10:24:34 -0700

    added shortcut methods to job model

--- DLPrototype/Extensions/Models/Job.swift
+++ DLPrototype/Extensions/Models/Job.swift
@@ -6,8 +6,22 @@
 //  Copyright © 2023 YegCollective. All rights reserved.
 //
 
+import SwiftUI
+
 extension Job {
     func id_int() -> Int {
         return Int(exactly: jid.rounded(.toNearestOrEven)) ?? 0
+    }
+
+    func colour_from_stored() -> Color {
+        if let c = colour {
+            return Color.fromStored(c)
+        }
+
+        return Color.clear
+    }
+
+    func fgColour() -> Color {
+        return self.colour_from_stored().isBright() ? .black : .white
     }
 }
