1f56f763a4ed79e09095657b173ed73332b15482
Ryan Priebe <rpriebe@me.com>
2024-01-01 14:45:12 -0700

    moved EntityViewMode to its own file

---
+++ DLPrototype/Enums/EntityViewMode.swift
@@ -0,0 +1,13 @@
+//
+//  EntityViewMode.swift
+//  DLPrototype
+//
+//  Created by Ryan Priebe on 2024-01-01.
+//  Copyright © 2024 YegCollective. All rights reserved.
+//
+
+import Foundation
+
+enum EntityViewMode {
+    case ready, create, update
+}
