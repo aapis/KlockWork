987e29b41d9044925ffe1e82c5f3829ef120ff79
Ryan Priebe <rpriebe@me.com>
2024-05-16 22:36:06 -0600

    updated CDNotes

---
+++ DLPrototype/Views/Dashboard/Widgets/IntroToKlockWork.swift
@@ -0,0 +1,19 @@
+//
+//  IntroToKlockWork.swift
+//  DLPrototype
+//
+//  Created by Ryan Priebe on 2024-05-16.
+//  Copyright © 2024 YegCollective. All rights reserved.
+//
+
+import SwiftUI
+
+struct IntroToKlockWork: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroToKlockWork()
+}
