d5e8ffd01467b6b30df9d800f00e5b2d2ad8eae7
Ryan Priebe <rpriebe@me.com>
2024-05-20 14:10:46 -0600

    simple content view

--- KlockWorkiOS/ContentView.swift
+++ KlockWorkiOS/ContentView.swift
@@ -9,22 +9,83 @@
 import SwiftUI
 import SwiftData
 
+struct NoteView: View {
+    public let note: Note
+    
+    @State private var versions: [NoteVersion] = []
+    @State private var current: NoteVersion? = nil
+    @State private var content: String = ""
+    @State private var title: String = ""
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            VStack {
+                HStack {
+                    Text(title)
+                        .font(.title)
+                    Spacer()
+                }
+            }
+            .padding()
+
+            HStack {
+                ScrollView(showsIndicators: false) {
+                    Text(content)
+                }
+                .padding()
+                Spacer()
+            }
+            .background(Theme.base)
+        }
+        .onAppear(perform: actionOnAppear)
+        .background(Theme.cPurple)
+//        .toolbar {
+//            ToolbarItem {
+//                Button(action: {}) {
+//                    Label("Versions", systemImage: "questionmark.circle")
+//                }
+//            }
+//        }
+    }
+}
+
+extension NoteView {
+    private func actionOnAppear() -> Void {
+        if let vers = note.versions {
+            versions = vers.allObjects as! [NoteVersion]
+            current = versions.first
+            
+            if let curr = current {
+                title = curr.title ?? "_NOTE_TITLE"
+                content = curr.content ?? "_NOTE_CONTENT"
+            }
+        } else if let body = note.body {
+            title = note.title ?? "_NOTE_TITLE"
+            content = body
+        }
+    }
+}
+
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+//    @Environment(\.modelContext) private var modelContext
+//    @Query private var items: [Item]
+    @Binding public var items: [Note]
 
     var body: some View {
         NavigationSplitView {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                        NoteView(note: item)
                     } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                        Text(item.title!)
                     }
                 }
                 .onDelete(perform: deleteItems)
             }
+            .navigationTitle("Notes")
+            .toolbarBackground(Theme.cPurple, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
@@ -43,20 +104,20 @@ struct ContentView: View {
     private func addItem() {
         withAnimation {
             let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
+//            modelContext.insert(newItem)
         }
     }
 
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             for index in offsets {
-                modelContext.delete(items[index])
+//                modelContext.delete(items[index])
             }
         }
     }
 }
 
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
-}
+//#Preview {
+//    ContentView()
+//        .modelContainer(for: Item.self, inMemory: true)
+//}
