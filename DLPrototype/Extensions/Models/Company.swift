adfa79ba5399ebba90180765cd6a6f8c09351172
Ryan Priebe <rpriebe@me.com>
2024-10-01 11:09:55 -0600

    added backgroundColour prop to Company

---
+++ DLPrototype/Extensions/Models/Company.swift
@@ -0,0 +1,19 @@
+//
+//  Company.swift
+//  DLPrototype
+//
+//  Created by Ryan Priebe on 2024-10-01.
+//  Copyright © 2024 YegCollective. All rights reserved.
+//
+
+import SwiftUI
+
+extension Company {
+    var backgroundColour: Color {
+        if let c = self.colour {
+            return Color.fromStored(c)
+        }
+
+        return Color.clear
+    }
+}
