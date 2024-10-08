0e2bc07077d30a7d884a83635ac1981223b5a7e4
Ryan Priebe <rpriebe@me.com>
2024-10-07 19:06:34 -0600

    allow scrolling in case names/abbreviations are super long

--- KlockWork/Views/Shared/EntityTypeHeader.swift
+++ KlockWork/Views/Shared/EntityTypeHeader.swift
@@ -14,16 +14,18 @@ struct EntityTypeHeader: View {
     @State private var parts: [Item] = []
 
     var body: some View {
-        HStack(alignment: .center, spacing: 8) {
-            if self.parts.count > 0 {
-                ForEach(self.parts, id: \.id) { part in part }
-            } else {
-                Item(
-                    text: "Choose a job from the sidebar, type into the field below. Enter/Return/+ to create records."
-                )
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .center, spacing: 8) {
+                if self.parts.count > 0 {
+                    ForEach(self.parts, id: \.id) { part in part }
+                } else {
+                    Item(
+                        text: "Choose a job from the sidebar, type into the field below. Enter/Return/+ to create records."
+                    )
+                }
             }
         }
-        .padding([.leading, .trailing])
+        .padding(.leading)
         .font(.title2)
         .onAppear(perform: self.actionSetViewState)
         .onChange(of: self.state.session.job) { self.actionSetViewState() }
@@ -46,10 +48,11 @@ struct EntityTypeHeader: View {
                     Text(self.text)
                         .underline(self.isHighlighted && self.target != .dashboard) // using .dashboard as "default"
                         .foregroundStyle((self.state.session.job?.backgroundColor ?? Theme.rowColour).isBright() ? Theme.base.opacity(0.55) : .white.opacity(0.55))
+                        .multilineTextAlignment(.leading)
                 }
                 .buttonStyle(.plain)
 
-                if self.text != self.state.session.job?.title && self.target != .dashboard {
+                if ![.jobs, .dashboard].contains(where: {$0 == self.target}) {
                     Image(systemName: "chevron.right")
                         .foregroundStyle((self.state.session.job?.backgroundColor ?? Theme.rowColour).isBright() ? Theme.base.opacity(0.3) : .white.opacity(0.3))
                 }
@@ -65,21 +68,21 @@ extension EntityTypeHeader {
     private func actionSetViewState() -> Void {
         self.parts = []
         if let job = self.state.session.job {
-            self.parts.append(Item(text: job.project?.company?.name ?? "", target: self.state.session.company?.pageDetailType ?? .dashboard))
-            self.parts.append(Item(text: job.project?.name ?? "", target: self.state.session.project?.pageDetailType ?? .dashboard))
+            self.parts.append(Item(text: job.project?.company?.abbreviation ?? job.project?.company?.name ?? "", target: self.state.session.company?.pageDetailType ?? .dashboard))
+            self.parts.append(Item(text: job.project?.abbreviation ?? job.project?.name ?? "", target: self.state.session.project?.pageDetailType ?? .dashboard))
             self.parts.append(Item(text: job.title ?? job.jid.string, target: job.pageDetailType))
         } else {
             if let company = self.state.session.company {
                 self.parts = []
                 if company.name != nil {
-                    self.parts.append(Item(text: company.name!, target: company.pageDetailType))
+                    self.parts.append(Item(text: company.abbreviation ?? company.name ?? "", target: company.pageDetailType))
                 }
             }
             if let project = self.state.session.project {
                 self.parts = []
                 if project.name != nil && project.company != nil {
-                    self.parts.append(Item(text: project.company!.name!, target: project.company!.pageDetailType))
-                    self.parts.append(Item(text: project.name!, target: project.pageDetailType))
+                    self.parts.append(Item(text: project.company!.abbreviation ?? project.company!.name ?? "", target: project.company!.pageDetailType))
+                    self.parts.append(Item(text: project.abbreviation ?? project.name ?? "", target: project.pageDetailType))
                 }
             }
         }
