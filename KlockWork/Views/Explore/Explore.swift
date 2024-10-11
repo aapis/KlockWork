2212c5f44db4b0406731158b8cf0c8b783dd8147
Ryan Priebe <rpriebe@me.com>
2024-10-11 01:18:30 -0600

    add initial (crappy) explore page

--- KlockWork/Views/Explore/Explore.swift
+++ KlockWork/Views/Explore/Explore.swift
@@ -10,7 +10,40 @@ import SwiftUI
 import KWCore
 
 struct Explore: View {
-    var body: some View {
-        Text("HI")
+    @EnvironmentObject public var state: Navigation
+    private var activities: [Activity] {
+        [
+            Activity(name: "Activity calendar", view: AnyView(Dashboard())),
+            Activity(name: "Flashcards", view: AnyView(Dashboard())),
+        ]
     }
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            UniversalHeader.StandaloneWidget(
+                type: .BruceWillis,
+                title: "Explore"
+            )
+
+            ForEach(self.activities, id: \.id) { activity in
+                Button {
+
+                } label: {
+                    Text(activity.name)
+                }
+                .buttonStyle(.plain)
+            }
+
+            Widgets()
+            Spacer()
+        }
+        .padding()
+        .background(Theme.toolbarColour)
+    }
+}
+
+struct Activity: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    var view: AnyView
 }
