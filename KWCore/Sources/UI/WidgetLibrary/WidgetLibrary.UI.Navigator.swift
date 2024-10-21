221114191c3db0d72ec2c64c8b7c8d4be8aef168
Ryan Priebe <rpriebe@me.com>
2024-10-21 15:46:59 -0600

    working on the folder view

--- KWCore/Sources/UI/WidgetLibrary/WidgetLibrary.UI.Navigator.swift
+++ KWCore/Sources/UI/WidgetLibrary/WidgetLibrary.UI.Navigator.swift
@@ -92,6 +92,7 @@ extension WidgetLibrary.UI {
 }
 
 extension WidgetLibrary.UI.Navigator {
+    // @TODO: merge Folders and List
     struct List: View {
         typealias UI = WidgetLibrary.UI
         typealias US = UI.UnifiedSidebar
@@ -101,6 +102,7 @@ extension WidgetLibrary.UI.Navigator {
         @AppStorage("widget.navigator.viewModeIndex") private var viewModeIndex: Int = 0
         @AppStorage("widget.navigator.sortModifiedOrder") private var navigatorSortModifiedOrder: Bool = false
         @AppStorage("widget.navigator.sortCreatedOrder") private var navigatorSortCreatedOrder: Bool = false
+        @AppStorage("widget.navigator.depth") private var depth: Int = 0
         @State private var companies: [Company] = []
         public let location: WidgetLocation = .content
 
@@ -108,6 +110,12 @@ extension WidgetLibrary.UI.Navigator {
             VStack(alignment: .leading, spacing: 1) {
                 VStack(spacing: 0) {
                     self.ListHeader
+                    UI.ResourcePath(
+                        company: self.state.session.company,
+                        project: self.state.session.project,
+                        job: self.state.session.job,
+                        showRoot: true
+                    )
                     self.ListLegend
                 }
 
@@ -121,7 +129,12 @@ extension WidgetLibrary.UI.Navigator {
 
 //                self.ListFooter
             }
-            .background(Theme.textBackground)
+            .background(
+                ZStack {
+                    self.state.session.appPage.primaryColour
+                    Theme.textBackground
+                }
+            )
             .onAppear(perform: self.actionOnAppear)
             .onChange(of: self.showPublished) { self.actionOnAppear() }
             .onChange(of: self.state.session.gif) { self.actionOnAppear() }
@@ -148,6 +161,8 @@ extension WidgetLibrary.UI.Navigator {
                     .background(Theme.textBackground)
                     .clipShape(.rect(cornerRadius: 5))
                 Spacer()
+                FancyGenericToolbar.ActionButton(icon: "arrow.up", callback: self.actionOnUp, helpText: "Up a directory")
+                    .keyboardShortcut(.upArrow, modifiers: [.command])
             }
             .padding(8)
             .background(self.state.session.appPage.primaryColour)
@@ -191,7 +206,6 @@ extension WidgetLibrary.UI.Navigator {
         }
 
         struct Row: View {
-            typealias UI = WidgetLibrary.UI
             @EnvironmentObject private var state: Navigation
             @AppStorage("widget.jobs.showPublished") private var showPublished: Bool = true
             @AppStorage("widget.navigator.viewModeIndex") private var viewModeIndex: Int = 0
@@ -254,10 +268,10 @@ extension WidgetLibrary.UI.Navigator {
 
                                 if self.state.session.job != nil {
                                     switch entity {
-                                    case .tasks: US.Tasks(job: self.state.session.job!, tasks: [])
-                                    case .records: US.Records(job: self.state.session.job!, records: [])
-                                    case .notes: US.Notes(job: self.state.session.job!, notes: [])
-                                    case .definitions: US.Definitions(job: self.state.session.job!, definitions: [])
+                                    case .tasks: US.Tasks(job: self.state.session.job!)
+                                    case .records: US.Records(job: self.state.session.job!)
+                                    case .notes: US.Notes(job: self.state.session.job!)
+                                    case .definitions: US.Definitions(job: self.state.session.job!)
                                     default: EmptyView()
                                     }
                                 }
@@ -315,16 +329,180 @@ extension WidgetLibrary.UI.Navigator {
     }
 
     struct Folders: View {
+        typealias UI = WidgetLibrary.UI
+        typealias US = UI.UnifiedSidebar
         @EnvironmentObject private var state: Navigation
+        @AppStorage("widget.jobs.showPublished") private var showPublished: Bool = true
+        @AppStorage("widget.navigator.altViewModeEnabled") private var altViewModeEnabled: Bool = true
+        @AppStorage("widget.navigator.viewModeIndex") private var viewModeIndex: Int = 0
+        @AppStorage("widget.navigator.sortModifiedOrder") private var navigatorSortModifiedOrder: Bool = false
+        @AppStorage("widget.navigator.sortCreatedOrder") private var navigatorSortCreatedOrder: Bool = false
+        @AppStorage("widget.navigator.depth") private var depth: Int = 0
+        @State private var companies: [Company] = []
+        @State private var projects: [Project] = []
+        @State private var jobs: [Job] = []
+        private let levels: [PageConfiguration.EntityType] = [.companies, .projects, .jobs]
+        public let location: WidgetLocation = .content
+        private var columns: [GridItem] {
+            return Array(repeating: .init(.flexible(minimum: 100)), count: 6)
+        }
 
         var body: some View {
-            VStack(alignment: .leading, spacing: 0) {
-                HStack {
-                    Text("Folder view to come")
-                    Spacer()
+            VStack(alignment: .leading, spacing: 1) {
+                VStack(spacing: 0) {
+                    self.ListHeader
+                    UI.ResourcePath(
+                        company: self.state.session.company,
+                        project: self.state.session.project,
+                        job: self.state.session.job,
+                        showRoot: true
+                    )
+                    self.ListLegend
                 }
-                .padding(8)
-                .background(self.state.session.appPage.primaryColour)
+
+                LazyVGrid(columns: columns, alignment: .leading) {
+                    switch self.depth {
+                    case 1:
+                        if self.projects.count > 0 {
+                            ForEach(self.projects, id: \.objectID) { project in
+                                Folder(entity: project)
+                            }
+                        }
+                    case 2:
+                        if self.jobs.count > 0 {
+                            ForEach(self.jobs, id: \.objectID) { job in
+                                Folder(entity: job)
+                            }
+                        }
+                    default:
+                        if self.companies.count  > 0 {
+                            ForEach(self.companies, id: \.objectID) { company in
+                                Folder(entity: company)
+                            }
+                        }
+                    }
+                }
+                .frame(maxWidth: 800)
+                .padding()
+
+//                self.ListFooter
+            }
+            .background(
+                ZStack {
+                    self.state.session.appPage.primaryColour
+                    Theme.textBackground
+                }
+            )
+            .onAppear(perform: self.actionOnAppear)
+            .onChange(of: self.showPublished) { self.actionOnAppear() }
+            .onChange(of: self.state.session.gif) { self.actionOnAppear() }
+            .onChange(of: self.depth) { self.actionOnAppear() }
+            .onChange(of: self.altViewModeEnabled) {
+                if self.altViewModeEnabled {
+                    self.viewModeIndex = 1
+                } else {
+                    self.viewModeIndex = 0
+                }
+            }
+        }
+
+        var ListHeader: some View {
+            HStack {
+                UI.Toggle("Published", isOn: $showPublished, icon: "checkmark.square", selectedIcon: "checkmark.square.fill")
+                    .help("Show or hide unpublished items")
+                    .padding(3)
+                    .background(Theme.textBackground)
+                    .clipShape(.rect(cornerRadius: 5))
+
+                UI.Toggle("Full Colour", isOn: $altViewModeEnabled, icon: "checkmark.square", selectedIcon: "checkmark.square.fill")
+                    .help("Navigator becomes colourful")
+                    .padding(3)
+                    .background(Theme.textBackground)
+                    .clipShape(.rect(cornerRadius: 5))
+                Spacer()
+
+                FancyGenericToolbar.ActionButton(icon: "arrow.up", callback: self.actionOnUp, helpText: "Up a directory")
+                    .keyboardShortcut(.upArrow, modifiers: [.command])
+            }
+            .padding(8)
+            .background(self.state.session.appPage.primaryColour)
+        }
+
+        @ViewBuilder var ListLegend: some View {
+            HStack {
+                Text("Name")
+                Spacer()
+                Button {
+                    self.navigatorSortModifiedOrder.toggle()
+                } label: {
+                    Text("Modified")
+                }
+                .buttonStyle(.plain)
+                Button {
+                    self.navigatorSortCreatedOrder.toggle()
+                } label: {
+                    Text("Created")
+                }
+                .buttonStyle(.plain)
+            }
+            .font(.caption)
+            .padding(4)
+            .background(
+                ZStack(alignment: .topLeading) {
+                    self.state.session.appPage.primaryColour
+                    LinearGradient(colors: [Theme.base.opacity(0.2), .clear], startPoint: .top, endPoint: .bottom)
+                        .blendMode(.softLight)
+                        .frame(height: 10)
+                }
+            )
+        }
+
+        var ListFooter: some View {
+            HStack {
+                Text("\(self.companies.count) Companies")
+                    .font(.caption)
+            }
+            .padding(4)
+        }
+
+        struct Folder: View {
+            @EnvironmentObject private var state: Navigation
+            @AppStorage("widget.jobs.showPublished") private var showPublished: Bool = true
+            @AppStorage("widget.navigator.viewModeIndex") private var viewModeIndex: Int = 0
+            @AppStorage("widget.navigator.sortModifiedOrder") private var navigatorSortModifiedOrder: Bool = false
+            @AppStorage("widget.navigator.sortCreatedOrder") private var navigatorSortCreatedOrder: Bool = false
+            @AppStorage("widget.navigator.depth") private var depth: Int = 0
+            public let entity: NSManagedObject
+            @State private var label: String = ""
+            @State private var lastModified: Date? = nil
+            @State private var created: Date? = nil
+            @State private var children: [NSManagedObject]? = nil
+            @State private var colour: Color = .white
+            @State private var relatedEntities: [PageConfiguration.EntityType] = []
+            @State private var isPresented: Bool = false
+            @State private var isHighlighted: Bool = false
+
+            var body: some View {
+                Button {
+                    self.actionOnTap()
+                    self.isPresented.toggle()
+                } label: {
+                    UI.IconBlock(type: .projects, text: self.label, colour: self.colour)
+                }
+                .help(self.label)
+                .buttonStyle(.plain)
+                .onAppear(perform: self.actionOnAppear)
+                .onChange(of: self.viewModeIndex) { self.actionOnAppear() }
+                .onChange(of: self.showPublished) { self.actionOnAppear() }
+            }
+
+            var ButtonContent: some View {
+                VStack {
+                    HStack {
+                        Image(systemName: "folder")
+                    }
+                    .font(.largeTitle)
+                }
             }
         }
     }
@@ -340,8 +518,23 @@ extension WidgetLibrary.UI.Navigator.List {
             planMembers: self.state.planning.companies
         )
     }
+
+    /// Fires when the Up button is tapped
+    /// - Returns: Void
+    private func actionOnUp() -> Void {
+        if self.depth == 0 {
+            self.state.session.company = nil
+        } else if self.depth == 1 {
+            self.depth = 0
+            self.state.session.project = nil
+        } else if self.depth == 2 {
+            self.depth = 1
+            self.state.session.job = nil
+        }
+    }
 }
 
+// @TODO: This and Folder are essentially identical, refactor
 extension WidgetLibrary.UI.Navigator.List.Row {
     /// Onload handler. Sets view state
     /// - Returns: Void
@@ -402,35 +595,140 @@ extension WidgetLibrary.UI.Navigator.List.Row {
     private func actionOnTap() -> Void {
         switch self.entity {
         case is Company:
-            self.state.session.company = nil
             let company = self.entity as! Company
-            self.state.session.company = company
+            self.state.session.company = self.state.session.company == company ? nil : company
             self.state.session.project = nil
             self.state.session.job = nil
         case is Project:
-            self.state.session.project = nil
             let project = self.entity as! Project
-            self.state.session.project = project
+            self.state.session.project = self.state.session.project == project ? nil : project
             self.state.session.job = nil
         case is Job:
-            self.state.session.job = nil
             let job = self.entity as! Job
-            self.state.session.setJob(job)
+            self.state.session.setJob(self.state.session.job == job ? nil : job)
         default:
             print("noop")
         }
     }
 }
 
-
 extension WidgetLibrary.UI.Navigator.Folders {
     /// Onload handler. Finds companies
     /// - Returns: Void
     private func actionOnAppear() -> Void {
-//        self.companies = CoreDataCompanies(moc: self.state.moc).all(
-//            allowKilled: self.showPublished,
-//            allowPlanMembersOnly: self.state.session.gif == .focus,
-//            planMembers: self.state.planning.companies
-//        )
+        self.companies = CoreDataCompanies(moc: self.state.moc).all(
+            allowKilled: self.showPublished,
+            allowPlanMembersOnly: self.state.session.gif == .focus,
+            planMembers: self.state.planning.companies
+        )
+
+        if self.companies.count(where: {$0.alive == true}) > 0 {
+            if let stored = self.state.session.company {
+                self.projects = CoreDataProjects(moc: self.state.moc).byCompany(stored)
+            }
+
+            if let stored = self.state.session.project {
+                self.jobs = CoreDataJob(moc: self.state.moc).byProject(stored)
+            }
+        } else {
+            self.companies = []
+            self.projects = []
+            self.jobs = []
+        }
+    }
+    
+    /// Fires when the Up button is tapped
+    /// - Returns: Void
+    private func actionOnUp() -> Void {
+        if self.depth == 0 {
+            self.state.session.company = nil
+        } else if self.depth == 1 {
+            self.depth = 0
+            self.state.session.project = nil
+        } else if self.depth == 2 {
+            self.depth = 1
+            self.state.session.job = nil
+        }
+    }
+}
+
+extension WidgetLibrary.UI.Navigator.Folders.Folder {
+    /// Onload handler. Sets view state
+    /// - Returns: Void
+    private func actionOnAppear() -> Void {
+        switch self.entity {
+        case is Company:
+            let company = self.entity as! Company
+            let projects = (company.projects?.allObjects as? [Project] ?? [])
+                .sorted(by: {
+                    if self.navigatorSortCreatedOrder {
+                        return $0.created ?? Date.now > $1.created ?? Date.now
+                    } else {
+                        return $0.lastUpdate ?? Date.now > $1.lastUpdate ?? Date.now
+                    }
+                })
+            self.label = company.name ?? "Error: Invalid company name"
+            self.children = projects
+            if self.showPublished {
+                self.children = projects
+                    .filter({$0.alive == true})
+            }
+            self.lastModified = company.lastUpdate
+            self.created = company.createdDate
+            self.colour = company.alive ? company.backgroundColor : .gray.opacity(0.7)
+            self.relatedEntities = [.people]
+            self.isPresented = company == self.state.session.company
+        case is Project:
+            let project = self.entity as! Project
+            let jobs = (project.jobs?.allObjects as? [Job] ?? [])
+                .sorted(by: {$0.lastUpdate ?? Date.now > $1.lastUpdate ?? Date.now})
+            self.label = project.name ?? "Error: Invalid project name"
+            self.children = jobs
+            if self.showPublished {
+                self.children = jobs
+                    .filter({$0.alive == true})
+            }
+            self.lastModified = project.lastUpdate
+            self.created = project.created
+            self.colour = project.alive ? project.backgroundColor : .gray.opacity(0.7)
+            self.isPresented = project == self.state.session.project
+        case is Job:
+            let job = self.entity as! Job
+            self.label = job.title ?? job.jid.string
+            self.children = []
+            self.lastModified = job.lastUpdate
+            self.created = job.created
+            self.colour = job.alive ? job.backgroundColor : .gray.opacity(0.7)
+            self.relatedEntities = [.tasks, .records, .notes, .definitions]
+            self.isPresented = job == self.state.session.job
+        default:
+            self.label = "Error: Invalid company name"
+            self.children = []
+        }
+    }
+
+    /// Fires when a row is tapped
+    /// - Returns: Void
+    private func actionOnTap() -> Void {
+        switch self.entity {
+        case is Company:
+            let company = self.entity as! Company
+            self.state.session.company = self.state.session.company == company ? nil : company
+            self.state.session.project = nil
+            self.state.session.job = nil
+            self.depth = 1
+        case is Project:
+            let project = self.entity as! Project
+            self.state.session.project = self.state.session.project == project ? nil : project
+            self.state.session.job = nil
+            self.depth = 2
+        case is Job:
+            let job = self.entity as! Job
+            self.state.session.setJob(self.state.session.job == job ? nil : job)
+        default:
+            print("noop")
+        }
+
+        print("DERPO onTapFolder depth=\(self.depth)")
     }
 }
