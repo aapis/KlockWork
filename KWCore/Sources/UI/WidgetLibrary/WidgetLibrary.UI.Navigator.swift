0f1c18dd8c2367c63c8a40d4fa43bbcb72ade8e0
Ryan Priebe <rpriebe@me.com>
2024-10-25 14:45:21 -0600

    use new selected entity style in navigator

--- KWCore/Sources/UI/WidgetLibrary/WidgetLibrary.UI.Navigator.swift
+++ KWCore/Sources/UI/WidgetLibrary/WidgetLibrary.UI.Navigator.swift
@@ -394,9 +394,9 @@ extension WidgetLibrary.UI.Navigator {
                         if self.isPresented {
                             self.colour.blendMode(.softLight)
                         }
-                        Image(systemName: self.isPresented ? "minus" : self.isHighlighted ? "folder.fill" : "folder")
+                        Image(systemName: self.isPresented ? "star.fill" : self.isHighlighted ? "folder.fill" : "folder")
                             // @TODO: create a ShapeStyle for this
-                            .foregroundStyle(self.isPresented ? .white : self.viewModeIndex == 1 ? self.colour.isBright() ? Theme.base : .white : self.colour)
+                            .foregroundStyle(self.isPresented ? .yellow : self.viewModeIndex == 1 ? self.colour.isBright() ? Theme.base : .white : self.colour)
                     }
                     .frame(width: 30, height: 30)
                     .cornerRadius(5)
