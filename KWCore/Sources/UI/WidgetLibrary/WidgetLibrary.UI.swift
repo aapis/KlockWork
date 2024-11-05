0fc82862ea4c1d081e5752024407e60859df0175
Ryan Priebe <rpriebe@me.com>
2024-11-05 14:54:14 -0700

    refactoring

--- KWCore/Sources/UI/WidgetLibrary/WidgetLibrary.UI.swift
+++ KWCore/Sources/UI/WidgetLibrary/WidgetLibrary.UI.swift
@@ -1407,44 +1407,6 @@ extension WidgetLibrary {
             }
         }
 
-        // MARK: RowActionButton
-        struct RowActionButton: View {
-            @EnvironmentObject public var state: Navigation
-            public var callback: (() -> Void)
-            public var icon: String?
-            public var iconAsImage: Image?
-            public var helpText: String = ""
-            public var highlightedColour: Color = .yellow
-            public var page: PageConfiguration.AppPage = .explore
-            @State private var isHighlighted: Bool = false
-
-            var body: some View {
-                Button {
-                    self.callback()
-                } label: {
-                    ZStack(alignment: .center) {
-                        LinearGradient(colors: [Theme.base, .clear], startPoint: .leading, endPoint: .trailing)
-                        self.isHighlighted ? self.highlightedColour : self.state.session.appPage.primaryColour
-
-                        if let icon = self.icon {
-                            Image(systemName: icon)
-                                .symbolRenderingMode(.hierarchical)
-                                .padding(5)
-                        } else if let iconAsImage = self.iconAsImage {
-                            iconAsImage
-                                .symbolRenderingMode(.hierarchical)
-                                .padding(5)
-                        }
-                    }
-                    .foregroundStyle(self.isHighlighted ? Theme.base : self.highlightedColour)
-                }
-                .font(.headline)
-                .buttonStyle(.plain)
-                .help(self.helpText)
-                .useDefaultHover({ hover in self.isHighlighted = hover })
-            }
-        }
-
         // MARK: Pagination
         struct Pagination: View {
             @EnvironmentObject public var state: Navigation
