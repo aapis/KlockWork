2e409876ebd972509e50addc0ce9c550d46c6da5
Ryan Priebe <rpriebe@me.com>
2024-11-12 14:58:12 -0700

    row view for Plan

--- KlockWork/Extensions/Models/Plan.swift
+++ KlockWork/Extensions/Models/Plan.swift
@@ -13,4 +13,17 @@ extension Plan {
     func isEmpty() -> Bool {
         return tasks!.count + jobs!.count + notes!.count == 0
     }
+
+    @ViewBuilder var rowView: some View {
+        HStack {
+            Image(systemName: "hexagon")
+            Text("Plan found")
+            Spacer()
+            Text(DateHelper.todayShort(self.created ?? Date(), format: "MMMM dd, yyyy"))
+                .foregroundStyle(.gray)
+        }
+        .padding(8)
+        .background(.gray)
+        .foregroundStyle(.white)
+    }
 }
